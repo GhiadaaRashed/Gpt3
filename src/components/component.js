c2a413b78aae95246f70a2a78fdd397767d82837
GhiadaaRashed <rashedghaidaa@gmail.com>
2024-05-01 10:17:46 +0300

    gpt3commit

---
+++ src/components/component.js
@@ -0,0 +1,5 @@
+export {default as Navbar} from './navbar/Navbar'
+export {default as Artictle} from './article/Artictle'
+export {default as Brand} from './brand/Brand'
+export {default as Cta} from './cta/Cta'
+export {default as Feature} from './feature/Feature'
